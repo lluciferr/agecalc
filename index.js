ac2b365d8eac8926b45c47313965013ac64232af
naran gyawali <nryng123@gmail.com>
2022-05-14 11:15:41 +0545

    year month and days calculated

---
+++ index.js
@@ -0,0 +1,38 @@
+var date = new Date();
+var today = [date.getFullYear(), date.getMonth() + 1, date.getUTCDate()];
+
+const dateOfB = [2001, 7, 29];
+const getTotalDays = () => {
+  return 30;
+};
+
+const getdiff = (dob) => {
+  var diff = [];
+  if (today[1] > dob[1]) {
+    diff[0] = today[0] - dob[0];
+    diff[1] = today[1] - dob[1];
+  } else {
+    diff[0] = today[0] - dob[0] - 1;
+    diff[1] = 12 - Math.abs(today[1] - dob[1]);
+  }
+
+  if (today[2] > dob[2]) {
+    diff[2] = today[2] - dob[2];
+  } else {
+    diff[1] = diff[1] - 1;
+    diff[2] = getTotalDays() - Math.abs(today[2] - dob[2]);
+  }
+
+  return diff
+};
+
+
+const agediff = getdiff(dateOfB)
+
+console.table(agediff)
+
+
+
+
+
+
